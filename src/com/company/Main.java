07e0abd3728fd6198eef0505dcfa9ec2eec95409
Shiuan1127 <dakotaylor1213@gmail.com>
2017-10-02 15:13:13 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String str;
+        char a,b;
+        str=in.next();
+        a=in.next().charAt(0);
+        b=in.next().charAt(0);
+        System.out.println(str.replace(a,b));
+    }
+}
